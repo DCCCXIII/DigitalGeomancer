5112c036a811a7bced7c88725160dcbe4de0c0f4
DCCCXIII <37632523+DCCCXIII@users.noreply.github.com>
2021-03-17 21:50:21 -0600

    Figure  type logic

--- DigitalGeomancer/DigitalGeomancer/Class1.cs
+++ DigitalGeomancer/DigitalGeomancer/Class1.cs
@@ -15,14 +15,120 @@ namespace DigitalGeomancer
 
         public FigureType Type 
         { 
-            get 
+            get
             {
-                //if (Parts[FigurePart.Head])
-                //{
+                return GetFigureType();
+            }
+        }
 
-                //}
-                return FigureType.Via;
-            } 
+        private FigureType GetFigureType()
+        {
+            if (Parts[FigurePart.Head])
+            {
+                if (Parts[FigurePart.Neck])
+                {
+                    if (Parts[FigurePart.Body])
+                    {
+                        if (Parts[FigurePart.Feet])
+                        {
+                            return FigureType.Via;
+                        }
+                        else
+                        {
+                            return FigureType.CaudaDraconis;
+                        }
+                    }
+                    else
+                    {
+                        if (Parts[FigurePart.Feet])
+                        {
+                            return FigureType.Puer;
+                        }
+                        else
+                        {
+                            return FigureType.FortunaMinor;
+                        }
+                    }
+                }
+                else
+                {
+                    if (Parts[FigurePart.Body])
+                    {
+                        if (Parts[FigurePart.Feet])
+                        {
+                            return FigureType.Puella;
+                        }
+                        else
+                        {
+                            return FigureType.Amissio;
+                        }
+                    }
+                    else
+                    {
+                        if (Parts[FigurePart.Feet])
+                        {
+                            return FigureType.Carcer;
+                        }
+                        else
+                        {
+                            return FigureType.Laetitia;
+                        }
+                    }
+                }
+            }
+            else
+            {
+                if (Parts[FigurePart.Neck])
+                {
+                    if (Parts[FigurePart.Body])
+                    {
+                        if (Parts[FigurePart.Feet])
+                        {
+                            return FigureType.CaputDraconis;
+                        }
+                        else
+                        {
+                            return FigureType.Conjunctio;
+                        }
+                    }
+                    else
+                    {
+                        if (Parts[FigurePart.Feet])
+                        {
+                            return FigureType.Acquisitio;
+                        }
+                        else
+                        {
+                            return FigureType.Rubeus;
+                        }
+                    }
+                }
+                else
+                {
+                    if (Parts[FigurePart.Body])
+                    {
+                        if (Parts[FigurePart.Feet])
+                        {
+                            return FigureType.FortunaMajor;
+                        }
+                        else
+                        {
+                            return FigureType.Albus;
+                        }
+                    }
+                    else
+                    {
+                        if (Parts[FigurePart.Feet])
+                        {
+                            return FigureType.Tristitia;
+                        }
+                        else
+                        {
+                            return FigureType.Populus;
+                        }
+                    }
+                }
+            }
         }
     }
 
