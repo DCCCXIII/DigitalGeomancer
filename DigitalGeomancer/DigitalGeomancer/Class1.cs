d1cbed9a9dc8be82c64ca64c42e15b3d91d6edcf
DCCCXIII <37632523+DCCCXIII@users.noreply.github.com>
2021-03-17 18:21:35 -0600

    Update Class1.cs
    Base info, need to set type

--- DigitalGeomancer/DigitalGeomancer/Class1.cs
+++ DigitalGeomancer/DigitalGeomancer/Class1.cs
@@ -1,8 +1,55 @@
 ﻿using System;
+using System.Collections.Generic;
 
 namespace DigitalGeomancer
 {
-    public class Class1
+    public class Figure
     {
+        public Dictionary<FigurePart, bool> Parts { get; set; } = new Dictionary<FigurePart, bool>()
+        {
+            {FigurePart.Head, false },
+            {FigurePart.Neck, false },
+            {FigurePart.Body, false },
+            {FigurePart.Feet, false }
+        };
+
+        public FigureType Type 
+        { 
+            get 
+            {
+                //if (Parts[FigurePart.Head])
+                //{
+
+                //}
+                return FigureType.Via;
+            } 
+        }
+    }
+
+    public enum FigurePart
+    {
+        Head,
+        Neck,
+        Body,
+        Feet
+    }
+     public enum FigureType
+    {
+        Puer,
+        Amissio,
+        Albus,
+        Populus,
+        FortunaMajor,
+        Conjunctio,
+        Puella,
+        Rubeus,
+        Acquisitio,
+        Carcer,
+        Tristitia,
+        Laetitia,
+        CaudaDraconis,
+        CaputDraconis,
+        FortunaMinor,
+        Via
     }
 }
